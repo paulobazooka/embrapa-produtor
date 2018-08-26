f77e1497af7b8e99f0da1335654af4a8045a6434
Bianquezi <psn.ads.ifsp@gmail.com>
2018-08-26 20:43:42 -0300

    desenvolvimento de tela de cadastro de usuários no perfi de administração

--- src/main/java/br/embrapa/produtor/controllers/AdministradorController.java
+++ src/main/java/br/embrapa/produtor/controllers/AdministradorController.java
@@ -1,14 +1,16 @@
 package br.embrapa.produtor.controllers;
 
+import br.embrapa.produtor.constants.TipoUsuario;
 import br.embrapa.produtor.models.Usuario;
 import br.embrapa.produtor.serviceimpl.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/administrador")
@@ -25,7 +27,18 @@ public class AdministradorController {
         mv.addObject("fragmento", "administracao");
 
         Usuario user = usuarioService.buscarPorEmail(principal.getName());
+
+        List<String> tipos = new ArrayList<>();
+
+        tipos.add(TipoUsuario.PRODUTOR.toString());
+        tipos.add(TipoUsuario.PESQUISADOR.toString());
+        tipos.add(TipoUsuario.ADMINISTRADOR.toString());
+
+       // tipos.forEach(tipo -> System.out.println(tipo));
+
+
         mv.addObject("user", user);
+        mv.addObject("tipos", tipos);
         return mv;
     }
 }
---
+++ src/main/resources/static/scripts/administracao.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    $('.modal').modal({
+        dismissible: false,
+    });
+
+});
+
