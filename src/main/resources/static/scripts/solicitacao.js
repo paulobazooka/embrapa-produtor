a3621297d8f11428414ebed6b76d14e7f589f083
Bianquezi <psn.ads.ifsp@gmail.com>
2018-08-26 19:40:08 -0300

    Permissão de apenas 6 fotos por solicitação

--- src/main/resources/static/scripts/solicitacao.js
+++ src/main/resources/static/scripts/solicitacao.js
@@ -7,18 +7,27 @@ $(document).ready(function(){
         $(this).formSelect();
     });
 
+    $('#limpar').click(function () {
+        $('#fotos').val('');
+        $('#btnCadastrar').prop("disabled", false);
+        $('#aviso').prop("class", "");
+    });
+
     $('#fotos').change(function () {
        var tamanho = $(this).val();
 
        if(tamanho.length > 6){
-           console.log("Maior que 6 arquivos");
+           $('#btnCadastrar').prop("disabled", true);
+           $('#aviso').prop("class", "red-text");
 
        }else{
-           if($('#btnCadastrar').prop(disabled) == "disabled"){
-               $('#btnCadastrar').removeProp("disabled");
+           if($('#btnCadastrar').prop("disabled", true)){
+               $('#btnCadastrar').prop("disabled", false);
+               $('#aviso').prop("class", "");
            }
        }
     });
+
 
     $('#btnNovaSolicitacao').click(function () {
         carregarUltimaPlantaSolicitada();
@@ -28,7 +37,7 @@ $(document).ready(function(){
 
 function carregarUltimaPlantaSolicitada() {
 
-    if ($('#ultimaplanta').text() != null){
+    if ($('#ultimaplanta').text() != ''){
         $('#cultura option:contains(' + $('#ultimaplanta').text() + ')').attr('selected', 'selected');
         $('#cultura').formSelect();
     }
